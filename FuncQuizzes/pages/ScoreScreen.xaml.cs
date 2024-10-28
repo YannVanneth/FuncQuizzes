09fc0b84ea84abc5537dcddda03f113c6be1dbaf
David Oeun <oeundavid235@gmail.com>
2024-10-29 01:49:03 +0700

    add score screen and change direct icon func from folder image to icons

--- FuncQuizzes/pages/ScoreScreen.xaml.cs
+++ FuncQuizzes/pages/ScoreScreen.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,17 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FuncQuizzes
+namespace FuncQuizzes.pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ScoreScreen.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ScoreScreen : Page
     {
-        public MainWindow()
+        public ScoreScreen()
         {
             InitializeComponent();
-            Main.Navigate(new pages.ScoreScreen());
         }
     }
 }
