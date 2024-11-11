2315a6c7fcdee8bee036380624b65af2649953c8
MarinaTaem <taemmarina@gmail.com>
2024-11-11 10:39:29 +0700

    added history

---
+++ FuncQuizzes/components/history_card.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FuncQuizzes.components
+{
+    /// <summary>
+    /// Interaction logic for history_card.xaml
+    /// </summary>
+    public partial class history_card : UserControl
+    {
+        public history_card()
+        {
+            InitializeComponent();
+        }
+    }
+}
