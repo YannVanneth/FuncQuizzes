e5db4cc3c33b26f08752e350406db1b388508dac
DAVID OEUN <oeun.david.1222@rupp.edu.kh>
2024-11-22 14:06:08 +0700

    fixed grade in history

--- FuncQuizzes/pages/History.xaml.cs
+++ FuncQuizzes/pages/History.xaml.cs
@@ -44,8 +44,7 @@ namespace FuncQuizzes.pages
                             string image = this.GetCategoryImage(dataTable.Rows[i]["category"].ToString()!);
 
                             double result;
-                            string GradeFetch = dataTable.Rows[i]["total_score"].ToString()!;
-
+                            string GradeFetch = GradeCalculate( double.Parse(dataTable.Rows[i]["total_score"].ToString()!));
                             double.TryParse(GradeFetch, out result);
 
                             GradeFetch = this.GetGradeImage(GradeFetch);
@@ -167,13 +166,32 @@ namespace FuncQuizzes.pages
 
         private string GradeCalculate(double score)
         {
-            if (score >= 90) return "A";
-            if (score >= 80) return "B";
-            if (score >= 70) return "C";
-            if (score >= 60) return "D";
-            if (score >= 50) return "E";
-            return "F";
+            if (score >= 90 && score <= 100)
+            {
+                return "A";
+            }
+            else if (score >= 80 && score < 90)
+            {
+                return "B";
+            }
+            else if (score >= 70 && score < 80)
+            {
+                return "C";
+            }
+            else if (score >= 60 && score < 70)
+            {
+                return "D";
+            }
+            else if (score >= 50 && score < 60)
+            {
+                return "E";
+            }
+            else
+            {
+                return "F";
+            }
         }
+
 
         private void Image_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
